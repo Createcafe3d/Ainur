41dae76673e87ce59f119f93d8d64e0f55b77a5f
Harakan <wgt538@gmail.com>
2016-07-24 12:45:52 -0600

    interum commit

--- src/musicfft.py
+++ src/musicfft.py
@@ -1,6 +1,17 @@
-import echonest.audio as audio
+import librosa
 
 class MusicFFT():
-    def __init__(songFile,resolution):
-        self._audio_file = audio.LocalAudioFile(songFile)
+    def __init__(self,songFile,resolution):
+        self._audio_file = librosa.util.example_audio_file()
+        self._audio = librosa.load(self._audio_file)
+        #self._audio_file = librosa.load(songFile)
+        print self._audio_file
+
+
+if __name__ == "__main__":
+    resolution = 1.0
+    song = '/home/green/Downloads/Lionel Richie - Hello.mp3'
+    song = '/home/green/Downloads/Lionel Richie - Hello.mp3'
+    MyClass = MusicFFT(song,resolution)
+
 
